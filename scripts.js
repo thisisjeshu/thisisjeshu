13582492dc29911d54f88f6b6a0db76f1c607ef5
Kolla Jeshurun <96195060+thisisjeshu@users.noreply.github.com>
2024-08-28 22:16:36 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,4 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const gallery = document.querySelector('.gallery');
-    const numberOfImages = 12; // Number of images to display
-
-    for (let i = 0; i < numberOfImages; i++) {
-        const img = document.createElement('img');
-        img.src = `https://source.unsplash.com/random/400x300?sig=${i}`;
-        img.alt = `Random Image ${i + 1}`;
-        gallery.appendChild(img);
-    }
      const menuToggle = document.getElementById('menuToggle');
     const navLinks = document.getElementById('navLinks');
 
