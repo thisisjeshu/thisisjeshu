4dc9a4dfafc57b8af156fc8465d47dc5efdcca29
Kolla Jeshurun <96195060+thisisjeshu@users.noreply.github.com>
2024-09-09 11:40:02 +0530

    Delete scripts.js

--- scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-     const menuToggle = document.getElementById('menuToggle');
-    const navLinks = document.getElementById('navLinks');
-
-    menuToggle.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
